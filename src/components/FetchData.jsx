395f76b3a1a545469a9f7c374b724f680ca617c1
Anselmo Dev <anselmoferrer.dev@gmail.com>
2025-01-25 00:14:47 -0300

    api fix

--- src/components/FetchData.jsx
+++ src/components/FetchData.jsx
@@ -15,7 +15,7 @@ export default function FetchAndStoreData() {
     //import.meta.env.VITE_API_KEY_12,
     //import.meta.env.VITE_API_KEY_13,
     import.meta.env.VITE_API_KEY_14,
-    import.meta.env.VITE_API_KEY_15,
+    //import.meta.env.VITE_API_KEY_15,
   ];
 
   const [error, setError] = useState(null);
